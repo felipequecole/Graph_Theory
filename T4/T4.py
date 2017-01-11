e07fadbd81f01e82e0173520c0d95556da607321
felipequecole <felipequecole@msn.com>
2017-01-11 21:22:32 +0000

    Dijkstra com concorrencia

---
+++ T4/T4.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+import networkx as nx
+from heapq import heappop, heappush
+import sys
+from matplotlib import pyplot as plt
+
+def dijkstra (G, root = '1'): 
+    push = heappush
+    pop = heappop
+    
+    aux = G.copy()
+    nodes = aux.nodes()
+    
+    for n in nodes:
+        # iniciar todos os vertices com lambda = infinito e sem predecessor
+        aux.node[n]['lambda'] = sys.float_info.max
+        aux.node[n]['pi'] = None
+    
+    aux.node[root]['lambda'] = 0
+    aux.node['2']['lambda'] = 0
+    aux.node['10']['lambda'] = 0
+
+    # colocar o lambda do vertice inicial como 0
+    
+    Q = [] # fila de prioridades
+    visited = []
+    
+    for n in nodes: 
+        push(Q, (aux.node[n]['lambda'], n))
+    
+    while Q:
+        u = pop(Q)       
+        u = u[1]
+        visited.append(u)
+        for v in aux.neighbors(u):
+            if v not in visited and aux.node[v]['lambda'] > (aux.node[u]['lambda'] + aux[u][v]['weight']):
+                # tiro atual da fila 
+                Q.remove((aux.node[v]['lambda'], v))
+                aux.node[v]['lambda'] = aux.node[u]['lambda'] + aux[u][v]['weight'] # atualiza peso
+                aux.node[v]['pi'] = u   # atualiza predecessor
+                push(Q, (aux.node[v]['lambda'], v)) # atualiza valores na fila
+    
+    mst = nx.Graph()
+    for v in aux.nodes():
+        mst.add_node(v)
+        if aux.node[v]['pi'] is not None: 
+            u = aux.node[v]['pi']
+            mst.add_edge(v, u)
+            mst[v][u]['weight'] = aux[v][u]['weight']
+    return mst 
+    
+def calcular_peso(T): 
+    peso = 0
+    pesos = nx.get_edge_attributes(T, 'weight')
+    for v in T.edges(): 
+        peso += pesos[v]
+    return peso
+        
+        
+
+def main ():
+    G = nx.read_weighted_edgelist('womenclub.edgelist')
+    H = dijkstra(G)
+    peso = calcular_peso(H)
+    print("Peso:",peso)
+    print(nx.is_forest(H))
+    pos = nx.spring_layout(H, k = 0.15, iterations=20)
+    nx.draw_networkx(H, pos)
+    dict_w = nx.get_edge_attributes(H,'weight')
+    print (dict_w)
+   
+    #nx.draw_networkx_edge_labels(H, pos, labels = dict_w, font_size = 8, label_pos = 0.5)
+    plt.savefig('dijkstra_test3.pdf')
+    plt.show()
+   
+  
+    
+if __name__ == '__main__': 
+    main()
